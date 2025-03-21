2f394dc5f48504aad24f4fbd801e5825507d1cc0
juwonleee <juwonlee0509@gmail.com>
2025-03-21 23:09:03 +0900

    Feat: 프로필 생성 API 연결

--- src/api/profileGenerate.js
+++ src/api/profileGenerate.js
@@ -1,1 +1,10 @@
-// empty file
+import axios from "axios";
+
+const generateProfile = (quizData) => {
+    const profileData = {
+        quizResult : quizData,
+    };
+  return axios.post(`https://server.couplegungjeon.store/api/v1/profile/generate`, profileData);
+};
+
+export default generateProfile;
