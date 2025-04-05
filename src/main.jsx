d2bc14e845e446daff05dda931367d84255e4876
clice lee <cliceleee@gmail.com>
2025-04-05 23:39:44 +0900

    Fix: CORS 405 오류 해결 - vercel.json에 API 프록시 및 CORS 헤더 설정 추가 - axiosConfig.js를 생성하여 API 요청에 CORS 헤더 적용 - 환경변수파일 설정 최적화 main.jsx에 설정 import 추가

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
+// axios 기본 설정 import
+import './api/axiosConfig';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
